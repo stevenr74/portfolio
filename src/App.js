9940d6909ed8fd3403e450bac9d8a9ccfb4e618c
Steven <steven.r987@gmail.com>
2021-10-13 11:59:22 -0600

    display container & css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import TopNav from './components/TopNav';
 import LinkBar from './components/LinkBar';
+import ProjectsContainer from './components/ProjectsContainer';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TopNav />
-          <LinkBar></LinkBar>
+        <LinkBar></LinkBar>
       </header>
+      <body>
+        <ProjectsContainer />
+      </body>
     </div>
   );
 }
---
+++ src/components/ProjectsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import grv2 from '../images/gamerecommenderv2.PNG'
+
+const ProjectsContainer = (props) => {
+
+    return (
+        <div className="displaybox">
+            <a href="https://stevenr74.github.io"><img src={grv2} alt="Game Recommender v2"/></a>
+        </div>
+    )
+}
+
+export default ProjectsContainer;
